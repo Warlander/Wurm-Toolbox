c053e8d1232716627046f5260ee86cd9e24ab355
Warlander <warlander@wurmonline.com>
2017-07-26 18:41:28 +0200

    Scaffolding

---
+++ js/site-javascript.js
@@ -0,0 +1,17 @@
+// <editor-fold desc="Setup and general functions">
+
+$(document).ready(function() {
+    window.onhashchange = onHashChange;
+});
+
+function onHashChange() {
+    console.log("Current hash: " + location.hash);
+}
+
+// </editor-fold>
+
+// <editor-fold desc="Deed Calculator">
+
+
+
+// </editor-fold>
